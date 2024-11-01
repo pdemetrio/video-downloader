1de86e3390cab87037c3476f6610f751dd916e46
Paulo RVS3D <paulo.demetrio@rvs3d.com>
2024-11-01 16:22:24 -0300

    alterações nos arquivos build e requirements

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+# build.sh
 
-# Instalar FFmpeg
-apt-get update
-apt-get install -y ffmpeg
+# Atualiza o pip
+pip install --upgrade pip
 
+# Instala as dependências do projeto
 pip install -r requirements.txt
+
+# Se você estiver usando Poetry, pode usar este comando em vez do pip
+# poetry install
+
+# Você pode adicionar outros comandos necessários para a preparação do seu ambiente
+# Por exemplo, criar diretórios necessários:
+mkdir -p downloads
+
+# Não inclua comandos que tentem modificar partes do sistema que são somente leitura
+# como apt-get update ou apt-get install
